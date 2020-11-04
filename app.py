cea43779aaeaf7cb8266bc2f1492c4033a0370e2
Kenta-morimori <takemori.kenta.official@gmail.com>
2020-11-04 14:43:48 +0900

    Add reply message
    Could not reflect it to LINE Developer yet

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=f”あなたは、{event.message.text}と言いました。"))
 
 
 if __name__ == "__main__":
